8fad4370e5063a32ec9859f442f98f4f0c7a86f6
michelle <90795143+michellebarrows@users.noreply.github.com>
2023-03-28 16:35:38 -0500

    water gets dirty over time

---
+++ Tiny Aquarium/Assets/Scripts/Simulation/WaterGrowth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterGrowth : MonoBehaviour
+{
+    public float growthTime = 2000f;
+
+    void Start()
+    {
+        //restore water state from save
+        Image image = GetComponent<Image>();
+        StartCoroutine(WaterGrowthRoutine(image));
+    }
+
+    IEnumerator WaterGrowthRoutine(Image image) {
+        float elapsedTime = 0.0f;
+        Color c = image.color;
+
+        while(elapsedTime < growthTime) {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            c.a = Mathf.Clamp01(elapsedTime / growthTime);
+            image.color = c;
+        }
+    }
+}
